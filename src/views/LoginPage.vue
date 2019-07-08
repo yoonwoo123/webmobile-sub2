311854be3661a39bb14973bb6dc087e446e046e8
tlswhd1020 <qkrtlswhd@naver.com>
2019-07-08 11:53:54 +0900

    S1P2212008-3 LoginPage(facebook function add)

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,13 +1,3 @@
-<template>
-  <v-layout align-center justify-center row fill-height>
-    <v-flex xs8 text-xs-center>
-      <v-layout align-center justify-center row fill-height elevation-5 style="min-height:500px;" white pa-4>
-        <v-flex xs12 text-xs-center>
-          <v-facebook-login app-id="966242223397117"></v-facebook-login>        </v-flex>
-      </v-layout>
-    </v-flex>
-  </v-layout>
-</template>
 
 <script>
 
