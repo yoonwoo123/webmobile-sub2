95cad953f83e140c949231453213334a44e0f1f6
tlswhd1020 <qkrtlswhd@naver.com>
2019-07-08 11:18:37 +0900

    LoginPage Update to facebook function

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -3,31 +3,23 @@
     <v-flex xs8 text-xs-center>
       <v-layout align-center justify-center row fill-height elevation-5 style="min-height:500px;" white pa-4>
         <v-flex xs12 text-xs-center>
-          <v-btn round color="#df4a31" dark v-on:click="loginWithGoogle" style="width:100%;"><v-icon size="25" class="mr-2">fa-google</v-icon> Google 로그인</v-btn>
-        </v-flex>
+          <v-facebook-login app-id="966242223397117"></v-facebook-login>        </v-flex>
       </v-layout>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import FirebaseService from '@/services/FirebaseService'
+
+import VFacebookLogin from 'vue-facebook-login-component'
+
+// OR, use cherry-pick export (better consistency)
 
 export default {
-	name: 'LoginPage',
-	data() {
-		return {}
-	},
-	components: {},
-	methods: {
-		async loginWithGoogle() {
-			const result = await FirebaseService.loginWithGoogle()
-			this.$store.state.accessToken = result.credential.accessToken
-			this.$store.state.user = result.user
-		}
-	},
-	mounted() {
-		console.log(this.$store.state)
-	}
+  name: 'LoginPage',
+  components: {
+    VFacebookLogin
+  }
 }
+
 </script>
