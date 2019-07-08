232b40bc966fac30fd9ac8a0a1d12e0ead111f1d
areygim <areygim@gmail.com>
2019-07-08 18:02:48 +0900

    S1P2212008-19 edited footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,8 +6,24 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
 	name: 'Footer',
-
+  data () {
+    return {
+      temperature: '',
+    }
+  },
+  methods: {
+    getTemperature: function () {
+      const kmaUrl = 'http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData?ServiceKey=1n75UtuOxfMND6RAspnfGNXQGtH%2BO1OkBn%2BGsOYO5aPZ4wtm3D%2FyXmC3tg9qNYbu18lGDMik4fL%2BT2jhJh%2BlqQ%3D%3D&base_date=20190708&base_time=1400&nx=1&ny=1'
+      axios.get(kmaUrl)
+        .then(response => console.log(response))
+    }
+  },
+  mounted: function () {
+    this.getTemperature()
+  },
 }
 </script>
