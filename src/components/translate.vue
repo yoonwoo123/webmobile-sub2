53fd89f9098d87093b18d4e96ba78044ae92b855
JO SEONG-WON <gogumassafy@gmail.com>
2019-07-09 17:26:20 +0900

    S1P2212008-28 Import node js, trnaslate

--- src/components/translate.vue
+++ src/components/translate.vue
@@ -3,44 +3,19 @@
 </template>
 
 <script>
-async function fetchData() {
-  var list = await getUserList();
-  console.log(list);
-}
+  import TranslateService from '@/services/TranslateService'
 
-function getUserList() {
-  return new Promise(function(resolve, reject) {
-    var userList = ['user1', 'user2', 'user3'];
-    resolve(userList);
-  });
-}
-
-async function quickstart(
-  projectId = 'blogdevelop-1562557566547' // Your GCP Project Id
-) {
-  // Imports the Google Cloud client library
-  const {Translate} = require('@google-cloud/translate');
-
-  // Instantiates a client
-  const translate = new Translate({projectId});
-
-  // The text to translate
-  const text = 'Hello, world!';
-
-  // The target language
-  const target = 'ru';
-
-  // Translates some text into Russian
-  const [translation] = await translate.translate(text, target);
-  console.log(`Text: ${text}`);
-  console.log(`Translation: ${translation}`);
-}
-
-fetchData();
-quickstart();
-export default {
-  name: 'TranslatePage'
-}
+  export default {
+    name: 'TranslatePage',
+    methods: {
+      async quickstart() {
+        await TranslateService.quickstart()
+      }
+    },
+    mounted () {
+      this.quickstart();
+    }
+  }
 </script>
 
 <style lang="css" scoped>
