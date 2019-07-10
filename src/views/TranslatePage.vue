0f21f90fce73bdee925eaff6131cca1add818eda
JO SEONG-WON <gogumassafy@gmail.com>
2019-07-10 10:44:14 +0900

    S1P2212008-29 Make button and done

--- src/views/TranslatePage.vue
+++ src/views/TranslatePage.vue
@@ -3,32 +3,9 @@
     <ImgBanner imgSrc="https://source.unsplash.com/1600x900/?sanfrancisco">
       <div style="line-height:1.2em;font-size:1.2em;" slot="text">Translate</div>
     </ImgBanner>
-    <v-container>
-      <v-form v-model="valid">
-        <v-container>
-          <v-layout justify-center>
-            <v-flex
-              xs6
-              sm12
-            >
-              <v-textarea
-                v-model="inputString"
-                placeholder="텍스트 입력"
-                required
-              ></v-textarea>
-              <v-textarea
-                placeholder="번역"
-                disabled
-                :value="result"
-              ></v-textarea>
-            </v-flex>
-          </v-layout>
-          <v-btn @click="">번역</v-btn>
-          <v-btn @click="clear">취소</v-btn>
-        </v-container>
-      </v-form>
-    </v-container>
+    <Translate></Translate>
   </div>
+
 </template>
 
 <script>
@@ -39,16 +16,6 @@ export default {
   components: {
     ImgBanner,
     Translate
-  },
-  data () {
-    return {
-      result: ''
-    }
-  },
-  methods: {
-    clear () {
-      this.result = ''
-    },
   }
 }
 </script>
