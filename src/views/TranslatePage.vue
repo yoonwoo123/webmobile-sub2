ea986c6bbb2375b630ec777037aa63a62db62198
YonungWoo <lkkjasd@naver.com>
2019-07-10 17:49:22 +0900

    translatepage 복원

---
+++ src/views/TranslatePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ImgBanner imgSrc="https://source.unsplash.com/1600x900/?sanfrancisco">
+      <div style="line-height:1.2em;font-size:1.2em;" slot="text">Translate</div>
+    </ImgBanner>
+    <Translate></Translate>
+  </div>
+
+</template>
+
+<script>
+import ImgBanner from '../components/ImgBanner'
+import Translate from '../components/Translate'
+export default {
+  name: 'TranslatePage',
+  components: {
+    ImgBanner,
+    Translate
+  }
+}
+</script>
+
+<style>
+
+</style>
