c8cb57ec1c9dfed2920c2dc7f89b8ff42ba09473
hyungyu <someth00@naver.com>
2019-07-11 09:39:23 +0900

    S1P2212008-22 hide gitlab graph mobile size, fix error

--- src/components/RepositoryList.vue
+++ src/components/RepositoryList.vue
@@ -5,7 +5,7 @@
       <Repository :repos="repositories[i - 1]"></Repository>
       <v-divider></v-divider>
     </v-flex>
-    <v-flex x12>
+    <v-flex xs12 class="hidden-xs-only">
       <div class="network-graph" data-url="./gitlab-network-graph/.data/data.json" data-commit-url="https://lab.ssafy.com/tlswhd1020/webmobile-sub2/commit/%s"></div>
     </v-flex>
   </v-layout>
